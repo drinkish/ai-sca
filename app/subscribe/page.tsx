4e2ca9c0be88a83f581c1f3a1cf92a77f6a9163d
drinkish <drinkishtown@gmail.com>
2024-10-17 22:46:54 +0100

    fixed stripe key

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
+import { useState } from 'react';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
