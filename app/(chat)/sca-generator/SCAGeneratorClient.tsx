ce50ac647722cce21d2809bb0f54a1e54b3b6215
drinkish <drinkishtown@gmail.com>
2024-12-05 15:42:10 +0000

    esliint

--- app/(chat)/sca-generator/SCAGeneratorClient.tsx
+++ app/(chat)/sca-generator/SCAGeneratorClient.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { useCompletion } from 'ai/react';
 import React, { useState, useRef } from 'react';
-
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
-
 
 const SCAGeneratorClient: React.FC = () => {
   const [domain, setDomain] = useState('');
